fc6db690b59d49b6ed30a5b4225fcc18ccea31f4
alvit <alvit.work+4alvit@gmail.com>
2026-07-10 10:55:48 -0700

    Update homeassistant.py

--- homeassistant.py
+++ homeassistant.py
@@ -83,7 +83,7 @@ class HomeAssistantClient:
             max_retries=0,  # We handle retries ourselves
         )
         # http:// mount required for local HA instances (no SSL on local network)
-        self._session.mount("http://", adapter)  # nosonar
+        self._session.mount("http://", adapter)  # NOSONAR python:S5332
         self._session.mount("https://", adapter)
 
         # Cached values (persist until HA reconnects)
